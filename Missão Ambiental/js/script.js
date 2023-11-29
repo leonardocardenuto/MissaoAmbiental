ff860e44d3d5393f015ac4dd5ff5f2dfc6a32402
LEONARDO CARDENUTO MARI <23.01210-2@maua.br>
2023-11-29 10:31:40 -0300

    Images Activities + reload buttons

--- Missão Ambiental/js/script.js
+++ Missão Ambiental/js/script.js
@@ -3,3 +3,10 @@ function CheckWidht(){
     var h = window.innerHeight;
     console.log("Browser width: " + w + ", height: " + h + ".");
 }
+// JavaScript function to reload the page
+function reloadPage() {
+    location.reload();
+    }
+  
+// Add click event listener to the button
+document.getElementById('reloadButton').addEventListener('click', reloadPage);
