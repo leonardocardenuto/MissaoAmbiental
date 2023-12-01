c56bcf53ae6e0acc22e1be0e9cc840177a239302
leonardocardenuto <leonardo.cardenutomari@gmail.com>
2023-12-01 11:18:14 -0300

    Images switching on homepage

--- Missão Ambiental/js/script.js
+++ Missão Ambiental/js/script.js
@@ -1,12 +1,8 @@
-function CheckWidht(){
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    console.log("Browser width: " + w + ", height: " + h + ".");
-}
-// JavaScript function to reload the page
-function reloadPage() {
-    location.reload();
-    }
-  
-// Add click event listener to the button
-document.getElementById('reloadButton').addEventListener('click', reloadPage);
+let img = document.getElementById("image-switch");
+let imgs = ["./images/imgPessoal.png", "./images/imgAgroecologia.jpeg"];
+let currentIndex = 0;
+
+setInterval(function () {
+  img.src = imgs[currentIndex];
+  currentIndex = (currentIndex + 1) % imgs.length;
+}, 8000);
