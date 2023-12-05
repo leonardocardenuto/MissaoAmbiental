39f26aadcf8a426dd6e5df8b09e0c948ae53c66f
leonardocardenuto <leonardo.cardenutomari@gmail.com>
2023-12-05 12:54:27 -0300

    Terms of use + css minor changes

--- Missão Ambiental/js/script.js
+++ Missão Ambiental/js/script.js
@@ -6,3 +6,27 @@ setInterval(function () {
   img.src = imgs[currentIndex];
   currentIndex = (currentIndex + 1) % imgs.length;
 }, 8000);
+
+var buttonClicked = false;
+
+        function openPopup() {
+            document.getElementById('overlay').style.display = 'block';
+            document.getElementById('popup').style.display = 'block';
+        }
+
+        function closePopup() {
+            document.getElementById('overlay').style.display = 'none';
+            document.getElementById('popup').style.display = 'none';
+            buttonClicked = true;
+            document.getElementById('termosCheckbox').disabled = !buttonClicked;
+        }
+
+        function checkScroll() {
+            var scrollableBox = document.querySelector('.scrollable-box');
+            var closeButton = document.getElementById('fecharButton');
+            if (scrollableBox.scrollHeight - scrollableBox.scrollTop === scrollableBox.clientHeight) {
+                closeButton.disabled = false;
+            } else {
+                closeButton.disabled = true;
+            }
+        }
